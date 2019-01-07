1094d0800e04277600a2c0489d121f10969f95d7
NoahDrucker <45659684+NoahDrucker@users.noreply.github.com>
2019-01-07 15:39:31 +0200

    Create csharp.cs

---
+++ csharp/csharp.cs
@@ -0,0 +1,13 @@
+using System; 
+using System.Net; 
+
+public class Example 
+{ 
+   public static void Main() 
+   { 
+      var client = new WebClient(); 
+      client.Proxy = new WebProxy("gate.smartproxy.io:7000"); 
+      client.Proxy.Credentials = new NetworkCredential("customer-username", "password"); 
+      Console.WriteLine(client.DownloadString("https://ipinfo.io")); 
+   } 
+}
