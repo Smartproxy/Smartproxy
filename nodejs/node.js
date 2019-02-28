15ba6d7a62933c3c15dc2f87531f575d5a4fc4e9
NoahDrucker <45659684+NoahDrucker@users.noreply.github.com>
2019-02-28 08:43:36 +0200

    Update node.js

--- nodejs/node.js
+++ nodejs/node.js
@@ -1,6 +1,13 @@
-require('request-promise')({ 
+require('request-promise') ( { 
 
-url: 'https://ipinfo.io/ip', 
-proxy: 'http://username:password@ip:port' 
+   url: 'https://ipinfo.io/ip', 
+   proxy: 'http://username:password@gate.smartproxy.com:7000' 
 
-}).then(function(data){ console.log(data); }, function(err){ console.error(err); });
+}).then( 
+   function(data ){ 
+      console.log(data); 
+    }, 
+    function(err){ 
+       console.error(err); 
+   } 
+);
