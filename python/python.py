5383aa9fb28d68a22ab6fde5723c603421a5d9fa
NoahDrucker <45659684+NoahDrucker@users.noreply.github.com>
2019-02-28 08:44:12 +0200

    Update python.py

--- python/python.py
+++ python/python.py
@@ -4,7 +4,7 @@ url = 'https://ipinfo.io'
 username = 'username'
 password = 'password’
 
-proxy = ('http://%s:%s@gate.smartproxy.io:7000' % (username, password))
+proxy = ('http://%s:%s@gate.smartproxy.com:7000' % (username, password))
 
 query = request.build_opener(request.ProxyHandler({'http': proxy, 'https': proxy}))
 
