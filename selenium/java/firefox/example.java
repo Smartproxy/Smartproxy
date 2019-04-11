5de66c163e92e1e7e2887f606476bf99e8f3b152
Noah <45659684+NoahDrucker@users.noreply.github.com>
2019-04-11 16:48:34 +0300

    Create example.java

---
+++ selenium/java/firefox/example.java
@@ -0,0 +1,43 @@
+import java.io.File;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.GeckoDriverService;
+
+public class Example {
+
+	public static void main(String[] args) {
+		
+		String ProxyServer = "gate.smartproxy.com";
+		int ProxyPort = 7000;
+		
+		String sHttpProxy = ProxyServer + ":" + ProxyPort;
+		
+		Proxy proxy = new Proxy();
+		
+		proxy.setHttpProxy(sHttpProxy);
+
+		GeckoDriverService service =new GeckoDriverService.Builder()
+								  .usingDriverExecutable(new File("C:\\Users\\Piero Guzman\\eclipse-workspace\\java\\driver\\geckodriver.exe"))
+								  .usingAnyFreePort()
+								  .usingAnyFreePort()
+								  .build();
+		FirefoxOptions options = new FirefoxOptions();
+		
+		options.setCapability("proxy", proxy);
+		
+		options.merge(options);    
+		
+		WebDriver driver=new FirefoxDriver(service, options);
+		driver.get("http://ip.smartproxy.com/");
+	    WebElement body = driver.findElement(By.tagName("body"));
+	    String bodyText = body.getText();
+		System.out.println(bodyText);
+
+	}
+
+}
