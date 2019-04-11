b9f8834b4cb4665e7d4c6d85bac1e57acd0afd9f
Noah <45659684+NoahDrucker@users.noreply.github.com>
2019-04-11 16:49:05 +0300

    Create example.java

---
+++ selenium/java/chrome/example.java
@@ -0,0 +1,43 @@
+package chromeScripts;
+
+import java.io.File;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Example {
+
+	public static void main(String[] args) {
+		
+		String ProxyServer = "gate.smartproxy.com";
+		int ProxyPort = 7000;
+		
+		String sHttpProxy = ProxyServer + ":" + ProxyPort;
+		
+		Proxy proxy = new Proxy();
+		
+		proxy.setHttpProxy(sHttpProxy);
+		
+		ChromeDriverService service = new ChromeDriverService.Builder()
+		                            .usingDriverExecutable(new File("C:\\Users\\Piero Guzman\\eclipse-workspace\\java\\driver\\chromedriver.exe"))
+		                            .usingAnyFreePort()
+		                            .build();
+		ChromeOptions options = new ChromeOptions();
+		
+		options.setCapability("proxy", proxy);
+		
+		options.merge(options);    
+		
+		WebDriver driver=new ChromeDriver(service, options);
+		driver.get("http://ip.smartproxy.com/");
+	    WebElement body = driver.findElement(By.tagName("body"));
+	    String bodyText = body.getText();
+		System.out.println(bodyText);
+	}
+
+}
