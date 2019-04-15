e954d6095105abbcb8c3f0934319b1d8fa4a39d8
Noah <45659684+NoahDrucker@users.noreply.github.com>
2019-04-15 17:01:45 +0300

    Update csharp.cs

--- csharp/csharp.cs
+++ csharp/csharp.cs
@@ -1,13 +1,47 @@
-using System; 
-using System.Net; 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Example 
-{ 
-   public static void Main() 
-   { 
-      var client = new WebClient(); 
-      client.Proxy = new WebProxy("gate.smartproxy.com:7000"); 
-      client.Proxy.Credentials = new NetworkCredential("customer-username", "password"); 
-      Console.WriteLine(client.DownloadString("https://ipinfo.io")); 
-   } 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Task t = new Task(DownloadPageAsync);
+        t.Start();
+        Console.ReadLine();
+    }
+
+    static async void DownloadPageAsync()
+    {
+        string page = "http://ip.smartproxy.com/";
+
+        var proxy = new WebProxy("gate.smartproxy.com:7000")
+        {
+            UseDefaultCredentials = false,
+
+            Credentials = new NetworkCredential(
+                userName: "username",
+                password: "password")
+        };
+
+        var httpClientHandler = new HttpClientHandler()
+        {
+            Proxy = proxy,
+        };
+
+        var client = new HttpClient(handler: httpClientHandler, disposeHandler: true);
+        var response = await client.GetAsync(page);
+        using (HttpContent content = response.Content)
+        {
+            string result = await content.ReadAsStringAsync();
+            Console.WriteLine(result);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        }
+    }
 }
