98939ff82c1ed495723f6edd49322503f57ee2ee
Maxim Eremenko <moeryomenko@gmail.com>
2019-04-16 12:54:30 +0300

    Add go integration
    Signed-off-by: Maxim Eremenko <moeryomenko@gmail.com>

---
+++ golang/golang.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+const (
+	resourceUrl = "https://ipinfo.io"
+	proxyHost   = "gate.smartproxy.com:7000"
+	username    = "username"
+	password    = "password"
+)
+
+func main() {
+	proxyUrl := &url.URL{
+		Scheme: "https",
+		User:   url.UserPassword(username, password),
+		Host:   proxyHost,
+	}
+
+	client := http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyUrl),
+		},
+	}
+
+	resp, err := client.Get(resourceUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	var body map[string]interface{}
+	if err = json.NewDecoder(resp.Body).Decode(&body); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(body)
+}
