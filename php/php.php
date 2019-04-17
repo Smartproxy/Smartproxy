97c41fafc0425184b4ece6419756ee6c22004d90
Noah <45659684+NoahDrucker@users.noreply.github.com>
2019-04-17 11:00:55 +0300

    Update php.php

--- php/php.php
+++ php/php.php
@@ -2,7 +2,7 @@
 $username = 'username'; 
 $password = 'password'; 
 $proxy = 'gate.smartproxy.com:7000'; 
-$target = curl_init('https://ipinfo.io/'); 
+$target = curl_init('http://ip.smartproxy.com/'); 
 curl_setopt($target, CURLOPT_RETURNTRANSFER, 1); 
 curl_setopt($target, CURLOPT_PROXY, $proxy); 
 curl_setopt($target, CURLOPT_PROXYUSERPWD, "$username:$password"); 
