ec7d150545cedfbf6e66f600ca3b94443795b7b5
konichuvak <arialinvlad@gmail.com>
2021-09-09 12:34:03 -0400

    Fixes broken authentication process in Python example

--- python/python.py
+++ python/python.py
@@ -1,11 +1,17 @@
 import requests
+from requests.auth import HTTPProxyAuth
 
 url = 'https://ipinfo.io'
 username = 'username'
 password = 'password'
 
-proxy = f'http://{username}:{password}@gate.smartproxy.com:7000'
-  
-response = requests.get(url, proxies={'http': proxy, 'https': proxy})
+proxy = f'http://gate.smartproxy.com:7000'
+
+response = requests.get(
+    url=url,
+    proxies={'http': proxy, 'https': proxy},
+    auth=HTTPProxyAuth(username, password)
+
+)
 
 print(response.text)
