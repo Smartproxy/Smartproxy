cfa4187d01a7cb1ab6bb956f4f500c3abf87706c
sebastianvilner <160615885+sebastianvilner@users.noreply.github.com>
2024-02-21 08:45:51 +0200

    Update nodesocks5h.js

--- nodejs/nodesocks5h.js
+++ nodejs/nodesocks5h.js
@@ -1,10 +1,10 @@
 const axios = require('axios');
 const SocksProxyAgent = require('socks-proxy-agent').SocksProxyAgent;
 
-const proxy = 'socks5h://spt2c4cxep:puh45DMWvx1fpfpy4D@gate.smartproxy.com:7000';
+const proxy = 'socks5h://username:password@gate.smartproxy.com:7000';
 const agent = new SocksProxyAgent(proxy);
 
-axios.get('https://ipinfo.io', { httpsAgent: agent })
+axios.get('https://ip.smartproxy.com/json', { httpsAgent: agent })
  .then(response => {
   console.log(response.data);
  })
