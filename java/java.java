77cbd16fe74a2d1cfcd71c0bbc3c00e45b41613b
4ofour <118349753+4ofour@users.noreply.github.com>
2024-10-28 12:27:49 +0200

    Update java.java

--- java/java.java
+++ java/java.java
@@ -1,54 +1,51 @@
-package org.example;
+package example;
 
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-import java.net.URL;
-import java.util.Base64;
+import java.io.*;
+import java.util.Random;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.*;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.*;
+import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
+import org.apache.http.util.EntityUtils;
 
-public class Main implements Runnable {
-    public static final String USERNAME = "SP_user";
-    public static final String PASSWORD = "SP_pass";
+class Client {
+    public static final String username = "SP_user";
+    public static final String password = "SP_pass";
+    public static final int port = 7000;
+    public CloseableHttpClient client;
 
-    public void run() {
-        try {
-            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("gate.smartproxy.com", 7000));
-            URL url = new URL("http://ip.smartproxy.com/json");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection(proxy);
-
-            // Encode credentials for Proxy-Authorization
-            String encodedCredentials = Base64.getEncoder().encodeToString((USERNAME + ":" + PASSWORD).getBytes());
-            connection.setRequestProperty("Proxy-Authorization", "Basic " + encodedCredentials);
-
-            connection.setRequestMethod("GET");
-
-            // Read the response
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            StringBuilder response = new StringBuilder();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-            // Print the response
-            System.out.println(response.toString());
-
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            System.exit(1);
-        }
-        System.exit(0);
+    public Client(String country) {
+        HttpHost proxy = new HttpHost("gate.smartproxy.com", port);
+        CredentialsProvider cred_provider = new BasicCredentialsProvider();
+        cred_provider.setCredentials(new AuthScope(proxy),
+            new UsernamePasswordCredentials(username, password));
+        client = HttpClients.custom()
+            .setConnectionManager(new BasicHttpClientConnectionManager())
+            .setProxy(proxy)
+            .setDefaultCredentialsProvider(cred_provider)
+            .build();
     }
 
-    public static void main(String[] args) {
-        new Thread(new Main()).start();
+    public String request(String url) throws IOException {
+        HttpGet request = new HttpGet(url);
+        CloseableHttpResponse response = client.execute(request);
+        try {
+            return EntityUtils.toString(response.getEntity());
+        } finally { response.close(); }
+    }
+
+    public void close() throws IOException { client.close(); }
+}
+
+public class Example {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Performing request(s)");
+        Client client = new Client(null);
+        try {
+            System.out.println(client.request("https://ip.smartproxy.com"));
+        } finally { client.close(); }
     }
 }
